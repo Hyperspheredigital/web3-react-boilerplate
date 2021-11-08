983c552ed8a6d5fd8477ebc7e968651faa643c72
gafotech <gafotech1@gmail.com>
2021-11-08 15:02:59 -0500

    feat(material-ui): add material ui library

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,16 +2,34 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Web3ReactProvider } from '@web3-react/core';
 import Web3 from 'web3';
+import React, { Fragment, useEffect } from 'react';
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { darkTheme, lightTheme } from '../src/theme';
 
 function getLibrary(provider: any) {
   return new Web3(provider);
 }
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles?.parentElement?.removeChild(jssStyles);
+    }
+  }, []);
+
   return (
-    <Web3ReactProvider getLibrary={getLibrary}>
-      <Component {...pageProps} />
-    </Web3ReactProvider>
+    <Fragment>
+      <ThemeProvider theme={darkTheme}>
+        <Web3ReactProvider getLibrary={getLibrary}>
+          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+          <CssBaseline />
+          <Component {...pageProps} />
+        </Web3ReactProvider>
+      </ThemeProvider>
+    </Fragment>
   );
 }
 
