74efa3cb87eda5a5a20671c64d6bd0302065f6f3
gafotech <gafotech1@gmail.com>
2021-11-10 13:40:26 -0500

    feat(web3-modal): add web3 modal example

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,13 @@
-import type { NextPage } from 'next';
-import { UnsupportedChainIdError, useWeb3React } from '@web3-react/core';
-import { injected } from '../components/wallet/connectors';
 import { Button } from '@mui/material';
+import WalletConnectProvider from '@walletconnect/web3-provider';
+import { providers } from 'ethers';
+import { useCallback, useEffect, useReducer } from 'react';
+import Web3Modal from 'web3modal';
+import { ellipseAddress, getChainData } from '../lib/utilities';
 import styled from 'styled-components';
-import Link from 'next/link';
 
 const Block = styled.div`
-  min-height: 100vh;
+  min-height: 80vh;
   padding: 4rem 0;
   flex: 1;
   display: flex;
@@ -15,52 +16,203 @@ const Block = styled.div`
   align-items: center;
 `;
 
-const Home: NextPage = () => {
-  const { active, account, library, connector, chainId, activate, deactivate, error } =
-    useWeb3React();
-  const isUnsupportedChainIdError = error instanceof UnsupportedChainIdError;
+const providerOptions = {
+  walletconnect: {
+    package: WalletConnectProvider, // required
+    options: {
+      infuraId: process.env.INFURA_ID, // required
+    },
+  },
+};
 
-  injected.on('error', (err) => {
-    console.log('on error', err);
+let web3Modal: Web3Modal;
+if (typeof window !== 'undefined') {
+  web3Modal = new Web3Modal({
+    network: 'mainnet', // optional
+    cacheProvider: true,
+    providerOptions, // required
   });
+}
 
-  async function connect() {
-    try {
-      let x = await activate(injected);
-    } catch (ex) {
-      console.log(ex);
-    }
-  }
+type StateType = {
+  provider?: any;
+  web3Provider?: any;
+  address?: string;
+  chainId?: number;
+};
 
-  async function disconnect() {
-    try {
-      deactivate();
-    } catch (ex) {
-      console.log(ex);
+type ActionType =
+  | {
+      type: 'SET_WEB3_PROVIDER';
+      provider?: StateType['provider'];
+      web3Provider?: StateType['web3Provider'];
+      address?: StateType['address'];
+      chainId?: StateType['chainId'];
     }
+  | {
+      type: 'SET_ADDRESS';
+      address?: StateType['address'];
+    }
+  | {
+      type: 'SET_CHAIN_ID';
+      chainId?: StateType['chainId'];
+    }
+  | {
+      type: 'RESET_WEB3_PROVIDER';
+    };
+
+const initialState: StateType = {
+  provider: null,
+  web3Provider: null,
+  address: '',
+  chainId: 1,
+};
+
+function reducer(state: StateType, action: ActionType): StateType {
+  switch (action.type) {
+    case 'SET_WEB3_PROVIDER':
+      return {
+        ...state,
+        provider: action.provider,
+        web3Provider: action.web3Provider,
+        address: action.address,
+        chainId: action.chainId,
+      };
+    case 'SET_ADDRESS':
+      return {
+        ...state,
+        address: action.address,
+      };
+    case 'SET_CHAIN_ID':
+      return {
+        ...state,
+        chainId: action.chainId,
+      };
+    case 'RESET_WEB3_PROVIDER':
+      return initialState;
+    default:
+      throw new Error();
   }
+}
+
+export const Connect = (): JSX.Element => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { provider, web3Provider, address, chainId } = state;
+
+  const connect = useCallback(async function () {
+    // This is the initial `provider` that is returned when
+    // using web3Modal to connect. Can be MetaMask or WalletConnect.
+    const provider = await web3Modal.connect();
+
+    // We plug the initial `provider` into ethers.js and get back
+    // a Web3Provider. This will add on methods from ethers.js and
+    // event listeners such as `.on()` will be different.
+    const web3Provider = new providers.Web3Provider(provider);
+    const signer = web3Provider.getSigner();
+    const address = await signer.getAddress();
+    const network = await web3Provider.getNetwork();
+
+    dispatch({
+      type: 'SET_WEB3_PROVIDER',
+      provider,
+      web3Provider,
+      address,
+      chainId: network.chainId,
+    });
+  }, []);
+
+  const disconnect = useCallback(
+    async function () {
+      await web3Modal.clearCachedProvider();
+      if (provider?.disconnect && typeof provider.disconnect === 'function') {
+        await provider.disconnect();
+      }
+      dispatch({
+        type: 'RESET_WEB3_PROVIDER',
+      });
+    },
+    [provider]
+  );
+
+  // Auto connect to the cached provider
+  useEffect(() => {
+    if (web3Modal.cachedProvider) {
+      connect();
+    }
+  }, [connect]);
+
+  // A `provider` should come with EIP-1193 events. We'll listen for those events
+  // here so that when a user switches accounts or networks, we can update the
+  // local React state with that new information.
+  useEffect(() => {
+    if (provider?.on) {
+      const handleAccountsChanged = (accounts: string[]) => {
+        // eslint-disable-next-line no-console
+        console.log('accountsChanged', accounts);
+        dispatch({
+          type: 'SET_ADDRESS',
+          address: accounts[0],
+        });
+      };
+
+      // https://docs.ethers.io/v5/concepts/best-practices/#best-practices--network-changes
+      const handleChainChanged = (_hexChainId: string) => {
+        window.location.reload();
+      };
+
+      const handleDisconnect = (error: { code: number; message: string }) => {
+        // eslint-disable-next-line no-console
+        console.log('disconnect', error);
+        disconnect();
+      };
+
+      provider.on('accountsChanged', handleAccountsChanged);
+      provider.on('chainChanged', handleChainChanged);
+      provider.on('disconnect', handleDisconnect);
+
+      // Subscription Cleanup
+      return () => {
+        if (provider.removeListener) {
+          provider.removeListener('accountsChanged', handleAccountsChanged);
+          provider.removeListener('chainChanged', handleChainChanged);
+          provider.removeListener('disconnect', handleDisconnect);
+        }
+      };
+    }
+  }, [provider, disconnect]);
+
+  const chainData = getChainData(chainId);
 
   return (
-    <>
-      <Block>
-        {active ? (
-          <div>
-            <p>
-              Connected with <b>{account}</b> on chain {chainId}
-            </p>
-            <Button onClick={disconnect} variant="contained">
-              Disconnect
-            </Button>
-          </div>
+    <Block>
+      <header>
+        {address && (
+          <ul>
+            <li>
+              <span className="mb-1">Network: </span>
+              <span>{chainData?.name}</span>
+            </li>
+            <li>
+              <span className="mb-1">Address: </span>
+              <span>{ellipseAddress(address)}</span>
+            </li>
+          </ul>
+        )}
+      </header>
+
+      <main>
+        {web3Provider ? (
+          <Button className="button" type="button" onClick={disconnect}>
+            Disconnect
+          </Button>
         ) : (
-          <Button onClick={connect} variant="contained">
-            Connect to MetaMask
+          <Button className="button" type="button" onClick={connect}>
+            Connect
           </Button>
         )}
-        {isUnsupportedChainIdError ? <p>Please switch to valid chain</p> : <p></p>}
-      </Block>
-    </>
+      </main>
+    </Block>
   );
 };
 
-export default Home;
+export default Connect;
